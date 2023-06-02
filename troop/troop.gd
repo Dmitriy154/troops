e9ff2bdca64ad83327ac75b8e67a65db047eec9c
Dmitriy154 <dmitriy154@gmail.com>
2023-06-02 16:56:07 +0300

    конец недели

--- troop/troop.gd
+++ troop/troop.gd
@@ -1,34 +1,51 @@
+class_name Troop
 extends CharacterBody2D
+#TROOP класс войска
 
-@export var speed = 100
+# состояния: активно (выделено), покой, прогулка, бег, атака, защита, отступление
+enum State {idle=0, walk=1, run=2, attack=3, defense=4, retreat=5}
 
-var target = Vector2.ZERO
+var state = State.idle
+var active: int = 0	#стадия активности, 1-клик по войску, 2-активно на след. кадр	
+var speed: float = 100
+var health: float = 100:
+	set(x): health = clamp(x,0,100)
+
+
+var target := Vector2.ZERO
+#ссылка на картинку - изменим Resource
+@onready var sprite2d = $Sprite2D		
 
 func _ready():
-	position = Vector2(100, 100)
-#	target = position
-#   hide()/show() - скрыть/показать текущий экземпляр
-
-func _input(event):
-	if event.is_action_pressed("click"):
-		target = get_global_mouse_position()
-		
-
+	pass
 
 func _physics_process(_delta):
-			
-	if Input.get_axis("ui_left", "ui_right") or Input.get_axis("ui_up", "ui_down"):
-		target = Vector2.ZERO
-		velocity = Vector2(
-			Input.get_axis("ui_left", "ui_right"),
-			Input.get_axis("ui_up", "ui_down")
-			).normalized() * speed
-		move_and_slide()
+	# если войско выделено щелчком, добавить условие - ждем второго щелчка!
+	if active == 2:
+		# ДВИЖЕНИЕ ПО КЛИКУ МЫШИ / если клик был внутри войска то return???
+		if not target == Vector2.ZERO:
+			velocity = position.direction_to(target) * speed
+			if position.distance_to(target) > 5:
+				move_and_slide()
+			else:
+				active = 0
+				$fon.hide()
 
-	if not target == Vector2.ZERO:
-		velocity = position.direction_to(target) * speed
-		#look_at(target)
-		if position.distance_to(target) > 10:
-			move_and_slide()
+		
+func _unhandled_input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+		if active == 1: active = 2
+	
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		active = 1
+		$fon.show()
+
+
+func death():
+	pass
+	#смерть отряда, удаляем экз-р  queue_free()
 
 
--- world/world.gd
+++ world/world.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
-##var Troop = preload("res://troop/troop.tscn")
+var troops_array: Array[Troop] = []
+@onready var troopScene = load("res://troop/troop.tscn")
 
 func _ready():
-	##var troop = Troop.instantiate()
-	##add_child(troop)
-	pass
+	for i in 6:
+		var troop = troopScene.instantiate()
+		troop.position = Vector2(50, 70*i+50)
+		troops_array.append(troop as Troop)
+		add_child(troop)
 
-func _input(event):
-	
-	if event.is_action_pressed("click"):
+
+func _unhandled_input(event):
+	if event.is_action_pressed("click"): 
+		pass
+		
 		$red_point.show()    
 		$red_point.position = get_global_mouse_position()
 		await get_tree().create_timer(1).timeout # ждем секунду
 		$red_point.hide()
-		
+
+
+
